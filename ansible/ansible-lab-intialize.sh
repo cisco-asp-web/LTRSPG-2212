59379f054354984c2b923b1f581c54d8f1808bcd
brmcdoug <brmcdoug@cisco.com>
2025-12-06 11:07:27 -0800

    init

--- ansible/ansible-lab-intialize.sh
+++ ansible/ansible-lab-intialize.sh
@@ -5,5 +5,10 @@ echo "startup dCloud script launched at: " > /home/dcloud/deploy.log
 date >> /home/dcloud/deploy.log
 whoami >> /home/dcloud/deploy.log
 
+echo "startup dCloud script launched at: " > /home/cisco/deploy.log
+
+date >> /home/cisco/deploy.log
+whoami >> /home/cisco/deploy.log
+
 ansible-playbook -e "ansible_user=dcloud ansible_ssh_pass=C1sco12345 ansible_sudo_pass=C1sco12345" \
   /home/dcloud/LTRSPG-2212/ansible/dcloud_startup_playbook.yaml -v >> /home/dcloud/deploy.log
